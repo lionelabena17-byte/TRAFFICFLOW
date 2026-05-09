cda85167f36c57190217c894d4eaabd164bdcb4e
KHALYX 2B <lionelabena17@gmail.com>
2026-05-09 11:53:43 +0100

    Fix gitignore et config

---
+++ config.js
@@ -0,0 +1,4 @@
+ const CONFIG = {
+     SUPABASE_URL: 'https://ctntssmflnwfbqykpxau.supabase.co',
+     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bnRzc21mbG53ZmJxeWtweGF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NTExNzcsImV4cCI6MjA5MjEyNzE3N30.zsSPzHXIOv_oVv5gTIWvuthZw7ZlcXrVzGxDfQh3wLk'
+}
